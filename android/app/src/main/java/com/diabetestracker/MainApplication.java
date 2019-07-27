93343d58470c84d03047d44b898a625046457408
puneet puneet <puneet1512@outlook.com>
2019-07-27 13:05:39 -0500

    Without loginwith Amazon

--- android/app/src/main/java/com/diabetestracker/MainApplication.java
+++ android/app/src/main/java/com/diabetestracker/MainApplication.java
@@ -3,7 +3,7 @@ package com.diabetestracker;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.reactlibrary.RNLoginWithAmazonPackage;
+import com.reactlibrary.RNAvsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -25,7 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new RNLoginWithAmazonPackage(),
+            new RNAvsPackage(),
             new RNGestureHandlerPackage()
       );
     }
--- app/features/home/containers/HomeView.js
+++ app/features/home/containers/HomeView.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import RNAvs from 'react-native-avs';
 
 export default class HomeView extends Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
-
+    testing = (()=>{
+        RNAvs;
+    })
     render() {
         return (
             <View>
